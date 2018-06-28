ce54600fdce92f1cb44cdcdf9cca7393b6d38b28
nivik13579 <nivik13579@gmail.com>
2018-06-28 14:38:09 +0530

    3

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello
 public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-	System.out.println("In Master Branch");
+	System.out.println("In Bug Branch");
    }
 }
